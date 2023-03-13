57f003e694c54fa245731d27c53700f1792d6824
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-03-13 08:43:55 -0600

    Tied Keys

--- MathContest/MathContestForm.Designer.vb
+++ MathContest/MathContestForm.Designer.vb
@@ -26,10 +26,15 @@ Partial Class MathContestForm
         Me.CurrentMathProblemGroupBox = New System.Windows.Forms.GroupBox()
         Me.MathProblemTypeGroupbox = New System.Windows.Forms.GroupBox()
         Me.ControlsGroupbox = New System.Windows.Forms.GroupBox()
-        Me.SubmitButton = New System.Windows.Forms.Button()
-        Me.ClearButton = New System.Windows.Forms.Button()
-        Me.SummaryButton = New System.Windows.Forms.Button()
         Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SummaryButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.SubmitButton = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.MathProblemTypeGroupbox.SuspendLayout()
         Me.ControlsGroupbox.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -46,16 +51,20 @@ Partial Class MathContestForm
         '
         Me.CurrentMathProblemGroupBox.Location = New System.Drawing.Point(12, 138)
         Me.CurrentMathProblemGroupBox.Name = "CurrentMathProblemGroupBox"
-        Me.CurrentMathProblemGroupBox.Size = New System.Drawing.Size(347, 326)
+        Me.CurrentMathProblemGroupBox.Size = New System.Drawing.Size(347, 303)
         Me.CurrentMathProblemGroupBox.TabIndex = 1
         Me.CurrentMathProblemGroupBox.TabStop = False
         Me.CurrentMathProblemGroupBox.Text = "Current Math Problem"
         '
         'MathProblemTypeGroupbox
         '
+        Me.MathProblemTypeGroupbox.Controls.Add(Me.RadioButton3)
+        Me.MathProblemTypeGroupbox.Controls.Add(Me.RadioButton4)
+        Me.MathProblemTypeGroupbox.Controls.Add(Me.RadioButton2)
+        Me.MathProblemTypeGroupbox.Controls.Add(Me.RadioButton1)
         Me.MathProblemTypeGroupbox.Location = New System.Drawing.Point(365, 138)
         Me.MathProblemTypeGroupbox.Name = "MathProblemTypeGroupbox"
-        Me.MathProblemTypeGroupbox.Size = New System.Drawing.Size(230, 326)
+        Me.MathProblemTypeGroupbox.Size = New System.Drawing.Size(230, 303)
         Me.MathProblemTypeGroupbox.TabIndex = 1
         Me.MathProblemTypeGroupbox.TabStop = False
         Me.MathProblemTypeGroupbox.Text = "Math Problem Type"
@@ -68,37 +77,10 @@ Partial Class MathContestForm
         Me.ControlsGroupbox.Controls.Add(Me.SubmitButton)
         Me.ControlsGroupbox.Location = New System.Drawing.Point(601, 12)
         Me.ControlsGroupbox.Name = "ControlsGroupbox"
-        Me.ControlsGroupbox.Size = New System.Drawing.Size(230, 452)
+        Me.ControlsGroupbox.Size = New System.Drawing.Size(230, 429)
         Me.ControlsGroupbox.TabIndex = 1
         Me.ControlsGroupbox.TabStop = False
         Me.ControlsGroupbox.Text = "Controls"
-        '
-        'SubmitButton
-        '
-        Me.SubmitButton.Location = New System.Drawing.Point(20, 26)
-        Me.SubmitButton.Name = "SubmitButton"
-        Me.SubmitButton.Size = New System.Drawing.Size(192, 94)
-        Me.SubmitButton.TabIndex = 0
-        Me.SubmitButton.Text = "Submit"
-        Me.SubmitButton.UseVisualStyleBackColor = True
-        '
-        'ClearButton
-        '
-        Me.ClearButton.Location = New System.Drawing.Point(20, 126)
-        Me.ClearButton.Name = "ClearButton"
-        Me.ClearButton.Size = New System.Drawing.Size(192, 94)
-        Me.ClearButton.TabIndex = 1
-        Me.ClearButton.Text = "Clear"
-        Me.ClearButton.UseVisualStyleBackColor = True
-        '
-        'SummaryButton
-        '
-        Me.SummaryButton.Location = New System.Drawing.Point(20, 226)
-        Me.SummaryButton.Name = "SummaryButton"
-        Me.SummaryButton.Size = New System.Drawing.Size(192, 94)
-        Me.SummaryButton.TabIndex = 2
-        Me.SummaryButton.Text = "Summary"
-        Me.SummaryButton.UseVisualStyleBackColor = True
         '
         'ExitButton
         '
@@ -109,17 +91,92 @@ Partial Class MathContestForm
         Me.ExitButton.Text = "Exit"
         Me.ExitButton.UseVisualStyleBackColor = True
         '
+        'SummaryButton
+        '
+        Me.SummaryButton.Location = New System.Drawing.Point(20, 226)
+        Me.SummaryButton.Name = "SummaryButton"
+        Me.SummaryButton.Size = New System.Drawing.Size(192, 94)
+        Me.SummaryButton.TabIndex = 2
+        Me.SummaryButton.Text = "Summary"
+        Me.SummaryButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(20, 126)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(192, 94)
+        Me.ClearButton.TabIndex = 1
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'SubmitButton
+        '
+        Me.SubmitButton.Location = New System.Drawing.Point(20, 26)
+        Me.SubmitButton.Name = "SubmitButton"
+        Me.SubmitButton.Size = New System.Drawing.Size(192, 94)
+        Me.SubmitButton.TabIndex = 0
+        Me.SubmitButton.Text = "Submit"
+        Me.SubmitButton.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(46, 82)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(121, 24)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "RadioButton1"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(46, 112)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(121, 24)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "RadioButton2"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(46, 172)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(121, 24)
+        Me.RadioButton3.TabIndex = 3
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "RadioButton3"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(46, 142)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(121, 24)
+        Me.RadioButton4.TabIndex = 2
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "RadioButton4"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
         'MathContestForm
         '
+        Me.AcceptButton = Me.SubmitButton
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(843, 476)
+        Me.CancelButton = Me.ClearButton
+        Me.ClientSize = New System.Drawing.Size(843, 453)
         Me.Controls.Add(Me.CurrentMathProblemGroupBox)
         Me.Controls.Add(Me.MathProblemTypeGroupbox)
         Me.Controls.Add(Me.ControlsGroupbox)
         Me.Controls.Add(Me.StudentInformationGroupBox)
         Me.Name = "MathContestForm"
         Me.Text = "MathContestForm"
+        Me.MathProblemTypeGroupbox.ResumeLayout(False)
+        Me.MathProblemTypeGroupbox.PerformLayout()
         Me.ControlsGroupbox.ResumeLayout(False)
         Me.ResumeLayout(False)
 
@@ -133,4 +190,8 @@ Partial Class MathContestForm
     Friend WithEvents SummaryButton As Button
     Friend WithEvents ClearButton As Button
     Friend WithEvents SubmitButton As Button
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
 End Class
